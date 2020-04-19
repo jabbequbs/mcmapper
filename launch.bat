c1da774bf2f59984c9b1b81765693f69aff9122e
jabbequbs <jabbequbs@users.noreply.github.com>
2020-04-19 17:54:59 -0600

    Updated launch script

--- launch.bat
+++ launch.bat
@@ -4,17 +4,14 @@ setlocal
 cd %~dp0
 
 set VENV=python3
-if not exist %VENV%\Scripts\activate.bat (
-    set PROMPT=
-    echo on
-    @echo Performing first time setup...
+set PYTHON=%VENV%\Scripts\python.exe
+if not exist %PYTHON% (
+    echo Performing first time setup...
+    echo py -3 -m venv %VENV%
     py -3 -m venv %VENV%
-    call %VENV%\Scripts\activate.bat
-    pip install -r scripts\requirements.txt
-    call %VENV%\Scripts\deactivate.bat
-    @echo off
+    echo %PYTHON% -m pip install -r scripts\requirements.txt
+    %PYTHON% -m pip install -r scripts\requirements.txt
 )
 
-call %VENV%\Scripts\activate.bat
-python scripts\viewer.py || pause
+%PYTHON% scripts\viewer.py || pause
 exit /b
