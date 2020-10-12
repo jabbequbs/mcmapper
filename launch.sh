21b72b90e3b8eff6e0c9e03af4a2c2e4dea0fbbb
jabbequbs <jabbequbs@users.noreply.github.com>
2020-10-12 12:45:00 -0600

    Updated Linux launch script

--- launch.sh
+++ launch.sh
@@ -4,9 +4,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
 if [ ! -d "python3" ]; then
     python3 -m venv python3
-    . python3/bin/activate
-    pip install -r scripts/requirements.txt
-    deactivate
+    ./python3/bin/python3 -m pip install -r scripts/requirements.txt
 fi
-. python3/bin/activate
-python3 scripts/viewer.py
+./python3/bin/python3 scripts/viewer.py
